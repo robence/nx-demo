fbf8b24bd3f8ac67794bfda578cc1e6c669eac36
Bence Rózsa <robence@gmail.com>
2024-05-18 09:15:53 +0000

    wip

--- org/file-validator/src/lib/file-validator.ts
+++ org/file-validator/src/lib/file-validator.ts
@@ -1,3 +1,22 @@
 export function fileValidator(): string {
   return 'file-validator'
 }
+
+
+export function extractVersionFromHeader (header: string) {
+  const versionMatch = header.match(/%PDF-(\d+\.\d+)/)
+  if (versionMatch) {
+    return parseFloat(versionMatch[1])
+  }
+  return null // Version not found in header
+}
+
+export function bufferToHeader (buffer: Buffer) {
+  // Convert buffer to string
+  const headerFromBuffer = buffer.toString('utf-8')
+
+  // Read the first 1024 characters for the header
+  const header = headerFromBuffer.substring(0, 1024)
+
+  return header
+}
